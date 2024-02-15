2c760825ca0bc181ebf16061708171ee923bbc8f
Yoshio Kaneko <kanekoyoshio007@gmail.com>
2024-02-15 16:56:22 +0900

    complete

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,30 @@
+class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
+  end
+
+  private
+
+  def bookmark_params
+    # whitelisting all the attributes the user can give us in the form
+    params.require(:bookmark).permit(:comment, :movie_id, :list)
+  end
+end
